180ccbfc34302491ce74c0756fa80a4e5dec89f2
xiaoyang00c <xiaoyang00c@126.com>
2017-11-08 20:23:17 +0800

    完成推进动画

---
+++ ZENDemo/Modeuls/Camera/Model/FilterData.h
@@ -0,0 +1,13 @@
+//
+//  FilterData.h
+//  ZENDemo
+//
+//  Created by Andy on 2017/11/8.
+//  Copyright © 2017年 Andy. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface FilterData : JSONModel
+
+@end
---
+++ ZENDemo/Modeuls/Camera/Model/FilterData.m
@@ -0,0 +1,13 @@
+//
+//  FilterData.m
+//  ZENDemo
+//
+//  Created by Andy on 2017/11/8.
+//  Copyright © 2017年 Andy. All rights reserved.
+//
+
+#import "FilterData.h"
+
+@implementation FilterData
+
+@end
